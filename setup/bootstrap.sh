25f67b1c3b42f266a4d21a9c40f00b42fd77ebe9
Akash Kothawale <akash@decached.com>
2014-01-21 20:15:50 +0530

    Will not fetch oh-my-zsh anymore. Modified theme

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -4,7 +4,8 @@ set -e
 Red="\033[1;31m"
 Gre="\033[1;32m"
 Cya="\033[1;36m"
-Whi="\033[4;97m"
+UWhi="\033[4;37m"
+UBWhi="\033[4;97m"
 RCol="\033[0m"
 
 DOTFILES=("vim" "vimrc" "zshrc" "gitconfig")
@@ -12,42 +13,25 @@ DOTFILES_DIR=dotfiles
 
 deps_check() {
     # Check for dependencies
-    echo -e "\n${Whi}Dependency Check${RCol}"
+    echo -e "\n${UBWhi}Dependency Check${RCol}"
     deps=("vim" "zsh" "git")
     FAIL=0
     for dep in ${deps[@]}
     do
-        echo -n "$dep"
+        echo -ne "${Cya}$dep ${RCol}"
         if [ `command -v $dep` ]; then
-            echo -e " ${Gre}✓ ${RCol}"
+            echo -e "${Gre}✓ ${RCol}"
         else
-            echo -e " ${Red}✗ ${RCol}"
+            echo -e "${Red}✗ ${RCol}"
             FAIL=1
         fi
     done
     return $FAIL
 }
 
-get_zsh() {
-    # Get oh-my-zsh
-    echo -e "\n${Whi}Looking for existing ~/.oh-my-zsh${RCol}"
-    ZSH_DIR=.oh-my-zsh
-    if [ -d $HOME/$ZSH_DIR ]; then
-        echo -e "${Cya}$ZSH_DIR ${RCol}exists, moving it to \
-            ${Cya}$ZSH_DIR-backup${RCol} ${Gre}✓ ${RCol}"
-        mv $ZSH_DIR $ZSH_DIR-backup
-    fi
-    wget --no-check-certificate \
-        https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
-}
-
-customize_zsh() {
-    cp -R $HOME/$DOTFILES_DIR/zsh/custom-oh-my-zsh/* $HOME/.oh-my-zsh
-}
-
 get_dotfiles() {
     # Clone dotfiles and deps
-    echo -e "\n${Whi}Looking for existing ~/$DOTFILES_DIR${RCol}"
+    echo -e "\n${UBWhi}Looking for existing ~/$DOTFILES_DIR${RCol}"
     if [ -d $HOME/$DOTFILES_DIR ]; then
         echo -e "${Cya}$DOTFILES_DIR ${RCol}exists, moving it to ${Cya}$DOTFILES_DIR-backup${RCol} ${Gre}✓ ${RCol}"
         mv $DOTFILES_DIR $DOTFILES_DIR-backup
@@ -58,14 +42,13 @@ get_dotfiles() {
 
 create_symlinks() {
     # Create symlinks for all files and dirs
-    echo -e "\n${Whi}Symlinks Creation${RCol}"
+    echo -e "\n${UBWhi}Symlinks Creation${RCol}"
     for FILE in ${DOTFILES[@]};
     do
         actual_path=$HOME/.$FILE
-        echo -e "${Cya}$FILE ${RCol}"
-
         if [ -d $actual_path ] || [ -f $actual_path ]; then
-            echo -e "$FILE already exists, moving it to $FILE-backup ${Gre}✓ ${RCol}"
+            echo -e "${Cya}$actual_path${RCol} already exists, moving to ${Cya}$actual_path-backup${RCol} ${Gre}✓ ${RCol}"
+
             mv $actual_path $(echo $actual_path-backup)
         fi
 
@@ -76,13 +59,16 @@ create_symlinks() {
 
 }
 
+customize_zsh() {
+    cp -R $HOME/$DOTFILES_DIR/zsh/custom-oh-my-zsh/* $HOME/.oh-my-zsh
+}
+
 deps_check
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 rm -rf $HOME/.*-backup # Remove backup files
-get_zsh
 get_dotfiles
 create_symlinks
 customize_zsh
