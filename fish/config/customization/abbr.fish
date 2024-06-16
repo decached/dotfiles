ab28519f586ca541136f5bc20a3ab54f7c0c8eec
Akash Kothawale <decached@gmail.com>
2024-06-16 15:03:08 -0700

    fish: Add abbrevations

---
+++ fish/config/customization/abbr.fish
@@ -0,0 +1,9 @@
+abbr -a lsa "ls --group-directories-first --color=auto -A"
+
+# misc
+abbr -a v vim
+abbr -a c "xclip -selection clipboard"
+abbr -a rgrep "grep -r --exclude-dir=.git --exclude-dir=env --exclude-dir=node_modules --color"
+abbr -a tree "tree -I '.git|env|node_modules|*.pyc|bower_components'"
+
+abbr -a gds "git diff --staged"
--- fish/config/init.fish
+++ fish/config/init.fish
@@ -1,3 +1,5 @@
 set -gx DOTFILES "$HOME/dotfiles"
 
+# A local set of fish commands that are host specific
+source $DOTFILES/fish/config/customization/abbr.fish
 source $HOME/.local.fish
