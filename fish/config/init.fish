b5ce74faffd48dec2a894b730c1942052ef00d60
Akash Kothawale <decached@gmail.com>
2024-06-29 03:38:32 -0700

    fish: Remove omf theme related customizations

--- fish/config/init.fish
+++ fish/config/init.fish
@@ -1,24 +1,11 @@
-set -gx DOTFILES "$HOME/dotfiles"
+set -gx DOTFILES $HOME/dotfiles
 set -gx EDITOR vim
 
 source $DOTFILES/fish/config/custom/abbr.fish
 
 set -g fzf_fd_opts --hidden --max-depth 5
 
-## Enable the time to be displayed.
-set -g theme_display_time yes
-set -g theme_display_group no
-set -g theme_display_hostname no
-set -g theme_display_batt no
-set -g theme_display_batt_icon no
-
-## Don't disable jobs indicator.
-set -g theme_display_jobs no
-
-## Always display the jobs indicator, even if there are no jobs.
-set -g theme_display_jobs_always yes
-set -g theme_color_user 045d56
-
-# A local set of fish commands that are host specific
-# source this file at the very end
-source $HOME/.local.fish
+# A local set of fish commands that are host specific source this file at the very end
+if test -f $HOME/.local.fish
+    source $HOME/.local.fish
+end
