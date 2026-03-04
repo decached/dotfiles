ed8c6cbb6a44e6dd0d50a7f7e31fa83eb5e82d31
Akash Kothawale <decached@gmail.com>
2026-03-03 23:19:21 -0800

    nvim: change colorscheme to vaporwave

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+    opts = {
+      styles = {
+        types = "NONE",
+        methods = "NONE",
+        numbers = "NONE",
+        strings = "NONE",
+        comments = "italic",
+        keywords = "bold,italic",
+        constants = "NONE",
+        functions = "italic",
+        operators = "NONE",
+        variables = "NONE",
+        parameters = "NONE",
+        conditionals = "italic",
+        virtual_text = "NONE",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vaporwave",
+    },
+  },
+}
