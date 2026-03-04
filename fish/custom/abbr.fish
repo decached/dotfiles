0eab461e1c475e244aff2e9198812954c6bae2f3
Akash Kothawale <decached@gmail.com>
2026-03-03 23:19:58 -0800

    fish: abbr v is now for nvim

--- fish/custom/abbr.fish
+++ fish/custom/abbr.fish
@@ -1,5 +1,5 @@
 # misc
-abbr -a v vim
+abbr -a v nvim
 abbr -a c "xclip -selection clipboard"
 abbr -a rgrep "grep -r --exclude-dir=.git --exclude-dir=env --exclude-dir=node_modules --color"
 abbr -a tree "tree -I '.git|env|node_modules|*.pyc|bower_components'"
