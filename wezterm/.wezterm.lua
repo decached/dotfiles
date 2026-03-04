0c29a3b657319842128b417354793ebee61f7a5f
Akash Kothawale <decached@gmail.com>
2026-03-03 23:20:21 -0800

    wezterm: Use CaskaydiaCove Nerd Font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,48 +5,47 @@
 -- Distributed under terms of the MIT license.
 --
 
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local act = wezterm.action
 
-wezterm.on('gui-startup', function(cmd)
-    local _, _, window = wezterm.mux.spawn_window(cmd or {})
-    window:gui_window():maximize()
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = wezterm.mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
 local configuration = {
-    --- behavior ---
-    enable_tab_bar = false,
-    warn_about_missing_glyphs = false,
-    window_close_confirmation = "NeverPrompt",
-    audible_bell = "Disabled",
-    max_fps = 75,
+	--- behavior ---
+	enable_tab_bar = false,
+	warn_about_missing_glyphs = false,
+	window_close_confirmation = "NeverPrompt",
+	audible_bell = "Disabled",
+	max_fps = 75,
 
-    --- window ---
-    window_padding = {
-        left = 1,
-        right = 1,
-        top = 0,
-        bottom = 0,
-    },
+	--- window ---
+	window_padding = {
+		left = 1,
+		right = 1,
+		top = 0,
+		bottom = 0,
+	},
 
-    --- appearence ---
-    color_scheme = 'Arthur',
-    line_height = 1.0,
-    font_size = 11.0,
-    initial_rows = 40,
-    font = wezterm.font('Source Code Pro'),
+	--- appearence ---
+	color_scheme = "Arthur",
+	line_height = 1.0,
+	font_size = 12.0,
+	initial_rows = 40,
+	font = wezterm.font("CaskaydiaCove Nerd Font"),
 
-    --- custom key bindings ---
-    keys = {
-        --- pane ---
-        { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Left', },
-        { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Right', },
-        { key = 'k', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Up', },
-        { key = 'j', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Down', },
-        { key = '|', mods = 'CTRL|SHIFT', action = act.SplitHorizontal {domain="CurrentPaneDomain"}},
-        { key = '_', mods = 'CTRL|SHIFT', action = act.SplitVertical {domain="CurrentPaneDomain"}},
-    },
-
+	--- custom key bindings ---
+	keys = {
+		--- pane ---
+		{ key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
+		{ key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
+		{ key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
+		{ key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
+		{ key = "|", mods = "CTRL|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "_", mods = "CTRL|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	},
 }
 
 return configuration
